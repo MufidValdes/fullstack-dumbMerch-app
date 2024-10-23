2f9fd0f673d9c588d537a64a1fc04089584f51c3
MufidValdes <muhamadmufidbachri27@gmail.com>
2024-10-23 18:47:31 +0700

    :tada: feat: initial commit

---
+++ server/server.ts
@@ -0,0 +1,9 @@
+import app from './src/app';
+// Load environment variables
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
