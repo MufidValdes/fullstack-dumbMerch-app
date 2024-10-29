b95ef21d1b2dafe7deaefc1ac93aa558af8c1cd3
MufidValdes <muhamadmufidbachri27@gmail.com>
2024-10-29 17:42:17 +0700

    :fire: feat: swagger API

--- server/src/utils/swaggerDoc.ts
+++ server/src/utils/swaggerDoc.ts
@@ -16,15 +16,45 @@ const doc = {
   },
   definitions: {
     Register: {
-      fullname: 'John Doe',
-      username: 'johndoe',
-      email: 'johndoe@example.com',
+      fullname: 'Antonio Gaming',
+      // username: 'antonio',
+      email: 'antonio@example.com',
       password: 'Password123',
     },
     Login: {
-      email: 'johndoe@example.com',
+      email: 'antonio@example.com',
       password: 'Password123',
     },
+
+    ProfileUpdate: {
+      fullname: 'antonio Gaming',
+      phone: '+123 456789',
+      address: 'Bintaro',
+      gender: 'MALE',
+      avatar: 'https://wallpapercave.com/wp/wp13357495.jpg',
+    },
+    Category: {
+      category_name: 'Electronics',
+    },
+    Product: {
+      product_name: 'Smartphone',
+      // product_image: 'https://thumb.viva.id/vivagadget/665x374/2023/11/14/655287073b173-xiaomi-14-pro_gadget.jpg',
+      description: 'A high-end smartphone with 128GB storage',
+      prince: 522.99,
+      stock: 50,
+      categoryId: 1,
+    },
+    Order: {
+      userId: 1,
+      orderItems: [
+        {
+          productId: 1,
+          quantity: 2,
+          price: 522.99,
+        },
+      ],
+    },
+    totalAmount: 1399.98,
   },
 };
 
